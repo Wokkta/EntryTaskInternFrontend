b03adff51a4e82bd7f16c73cc8329f4ce6f277e2
Wokkta <55146833+Wokkta@users.noreply.github.com>
2023-10-08 08:18:51 +0300

    Update index.js

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,21 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js';
 
-console.log("Let's rock")
-console.log(encoded, translations)
+console.log("Let's rock");
+console.log(encoded, translations);
 
+const variants = new Set();
+const decoded = encoded.map((el) => {
+  for (let i of Object.keys(el)) {
+    const match = i.match(/\bid|Id\b/);
+    if (match) {
+      const decodedValue = translations[el[i]];
+      variants.add(+el[i]);
+      el[i] = decodedValue;
+    }
+  }
+  return el;
+});
 
+console.log(decoded);
+console.log('Уникальные айди :', [...variants]);
 
-// console.log(decoded)
